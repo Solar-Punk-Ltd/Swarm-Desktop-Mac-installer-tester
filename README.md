daa02c75374f437b7614ace31b261ab76d2d824e
Zoltán Mihály <zolmac@Zoltans-MacBook-Pro.local>
2023-10-18 17:48:59 +0200

    warning, cleaning

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Swarm-Desktop-Mac-installer-tester
+
+Before run this tester, DELETE SWARM DESKTOP app from /Applications, and IF YOU HAVE ANY FUNDS in your wallet backup them - https://docs.ethswarm.org/docs/desktop/backup-restore
+
+This tester cleans up every downloads, installations and created directories/files. This means every previous downloads, installations and created directories/files can be deleted.
+
+It is HIGHLY RECOMMENDED using this tester in a separated enviroment, where no previously installed Swarm Desktop can be found
--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"strings"
 )
 
 var steps, interval int = 1, 10
@@ -24,39 +22,33 @@ func main() {
 			fmt.Println("ERROR time interval:", interval_err.Error())
 		}
 
-		fmt.Println("THIS IS SWARM INSTALLER TESTER")
-		fmt.Println("Before run this app, DELETE SWARM DESKTOP app from /Application, and IF YOU HAVE ANY FUNDS in your wallet backup it - https://docs.ethswarm.org/docs/desktop/backup-restore")
-		fmt.Println("Type OK, if you are ready!")
+		input, werr := warning()
 
-		reader := bufio.NewReader(os.Stdin)
-		str, str_err := reader.ReadString('\n')
-		input := strings.Trim(str, "\n")
-		ok := "OK"
-
-		if str_err != nil {
-			fmt.Println("ERROR:", str_err.Error())
+		if werr != nil {
+			fmt.Println("ERROR:", werr.Error())
 		} else {
-			if input == ok {
+			if input == "OK" {
 				fmt.Println("Let's run")
 				myInterval(steps, interval)
 			} else {
 				fmt.Println("Try again")
 			}
 		}
+
 	} else {
-		fmt.Println("Missing iteration and/or interval argument")
-		fmt.Println("Running test only once")
-		myInterval(steps, interval)
+		input, werr := warning()
+
+		if werr != nil {
+			fmt.Println("ERROR:", werr.Error())
+		} else {
+			if input == "OK" {
+				fmt.Println("Missing iteration and/or interval argument")
+				fmt.Println("Running test only once")
+				myInterval(steps, interval)
+			} else {
+				fmt.Println("Try again")
+			}
+		}
 	}
-	/*
-	   beeLog, beeLogErr := os.ReadFile("/Users/zolmac/Library/Logs/Swarm Desktop/bee.current.log")
 
-	   	if beeLogErr != nil {
-	   		//log.Fatal(beeLogErr)
-	   		fmt.Println(beeLogErr)
-	   	} else {
-
-	   		checkBeeLog(string(beeLog))
-	   	}
-	*/
 }
